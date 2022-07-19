8d27491f4ef2a05364069498151214d688e3b041
Rodrigo Carrasco <rodrigo.carrasco40@inacapmail.cl>
2022-07-19 15:48:18 -0400

    para evaluacion 2

--- ecommerce-react/src/data.js
+++ ecommerce-react/src/data.js
@@ -7,15 +7,15 @@ const data = {
             quantity: "20",
             price: "43990",
             img: "https://www.games-workshop.com/resources/catalog/product/920x950/60030101049_EngSMCodex01.jpg",
-            desc: "98 hojas de datos que cubren las unidades disponibles para cada Capítulo."
+            desc: "98 hojas de datos que cubren las unidades disponibles para los Marines Espaciales."
         },
         {
             id: "2",
             title: "Patrulla de Combate: Marines Espaciales",
             quantity: "8",
-            price: "109990",
+            price: "109.990",
             img: "https://www.games-workshop.com/resources/catalog/product/920x950/99120101280_SMCombatPatrolGroup.jpg",
-            desc: "2022-07-18"
+            desc: "18 miniaturas de plástico, ¡diseñada para ahorrarte dinero!"
         },
         {
             id: "3",
@@ -23,7 +23,7 @@ const data = {
             quantity: "45",
             price: "31990",
             img: "https://www.games-workshop.com/resources/catalog/product/920x950/99070101077_SMCPTGravisArmourLead.jpg",
-            desc: "2022-07-18"
+            desc: "Una elección de Cuartel General indomable para tu ejército de Marines Espaciales"
         },
         {
             id: "4",
@@ -31,7 +31,7 @@ const data = {
             quantity: "3",
             price: "30990",
             img: "https://www.games-workshop.com/resources/catalog/product/920x950/99070101048_SMCaptainMasterCraftedBoltRifleLead.jpg",
-            desc: "2022-07-18"
+            desc: "Una potente opción de Cuartel General disponible para los Marines Espaciales"
         },
         {
             id: "5",
@@ -39,7 +39,7 @@ const data = {
             quantity: "16",
             price: "49990",
             img: "https://www.games-workshop.com/resources/catalog/product/920x950/99120101190_PrimarisIntercessors01.jpg",
-            desc: "2022-07-18"
+            desc: "Kit de plástico multicomponente contiene las piezas necesarias para montar una escuadra de 10 Primaris Intercessors"
         },
         {
             id: "6",
@@ -47,7 +47,23 @@ const data = {
             quantity: "21",
             price: "29990",
             img: "https://www.games-workshop.com/resources/catalog/product/920x950/99120101128_SpaceMarineTacticalSquadReformat01.jpg",
-            desc: "2022-07-18"
+            desc: "Escuadron que cubre terreno, proporciona fuego de apoyo y se lanzan al cuerpo a cuerpo, según dicten las necesidades del momento."
+        },
+        {
+            id: "7",
+            title: "Predator Destructor",
+            quantity: "5",
+            price: "55990",
+            img: "https://www.games-workshop.com/resources/catalog/product/920x950/99120101247_SMPredator01Alt.jpg",
+            desc: "Pulveriza a tus enemigos con potencia de fuego de largo alcance."
+        },
+        {
+            id: "8",
+            title: "Datacards: Space Marines (English)",
+            quantity: "2",
+            price: "21990",
+            img: "https://www.games-workshop.com/resources/catalog/product/920x950/60050101002_EngSMDatacards01.jpg",
+            desc: "61 tarjetas de datos con estratagemas, letanias de batalla y poderes psíquicos."
         }
     ]};
 
